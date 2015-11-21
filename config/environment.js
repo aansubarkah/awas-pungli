e8376edfb633cc84b7c093b998d9618f5f8c1f7d
aansubarkah <aansubarkah@gmail.com>
2015-11-21 13:50:11 +0700

    add pods

--- config/environment.js
+++ config/environment.js
@@ -1,47 +1,65 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'awas-pungli',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
+	var ENV = {
+		modulePrefix: 'awas-pungli',
+		podModulePrefix: 'awas-pungli/pods',
+		environment: environment,
+		baseURL: '/',
+		locationType: 'auto',
+		EmberENV: {
+			FEATURES: {
+				// Here you can enable experimental features on an ember canary build
+				// e.g. 'with-controller': true
+			}
+		},
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
-  };
+		APP: {
+			// Here you can pass flags/options to your application instance
+			// when it is created
+		},
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+		contentSecurityPolicy: {
+			'default-src': "'none'",
+			'script-src': "'self' 'unsafe-inline' 'unsafe-eval' https://cdn.datatables.net https://code.jquery.com https://maxcdn.bootstrapcdn.com https://cdnjs.cloudflare.com *.googleapis.com maps.gstatic.com",
+			'font-src': "'self' https://maxcdn.bootstrapcdn.com https://cdn.datatables.net http://fonts.gstatic.com https://fonts.gstatic.com https://code.ionicframework.com",
+			'connect-src': "'self' http://localhost:8765 http://api.awaspungli.com",
+			'img-src': "'self' *.googleapis.com maps.gstatic.com csi.gstatic.com *.twimg.com",
+			'style-src': "'self' 'unsafe-inline' https://code.ionicframework.com https://maxcdn.bootstrapcdn.com https://cdn.datatables.net fonts.googleapis.com map.gstatic.com",
+			'media-src': "'self'"
+		},
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+		googleMap: {
+			libraries: ['places', 'geometry'],
+			//apiKey: 'AIzaSyCNNx7nk22X892vSkB9C5pIChfl4wAb6c8'//on production
+			apiKey: 'AIzaSyAwKFM_JN6cwBiSn13ALJVuoR9gcvAW1OE'//on localhost
+		}
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+	};
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+	if (environment === 'development') {
+		// ENV.APP.LOG_RESOLVER = true;
+		// ENV.APP.LOG_ACTIVE_GENERATION = true;
+		// ENV.APP.LOG_TRANSITIONS = true;
+		// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+		// ENV.APP.LOG_VIEW_LOOKUPS = true;
+	}
 
-  if (environment === 'production') {
+	if (environment === 'test') {
+		// Testem prefers this...
+		ENV.baseURL = '/';
+		ENV.locationType = 'none';
 
-  }
+		// keep test console output quieter
+		ENV.APP.LOG_ACTIVE_GENERATION = false;
+		ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-  return ENV;
+		ENV.APP.rootElement = '#ember-testing';
+	}
+
+	if (environment === 'production') {
+
+	}
+
+	return ENV;
 };
